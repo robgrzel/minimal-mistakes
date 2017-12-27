8d04455a71cf7fc91361c4c0d38bbaac1ab63ece
Robert Grzelka <robert.grzelka@outlook.com>
2017-12-27 19:43:03 +0100

    images

--- RunInDocker.ps1
+++ RunInDocker.ps1
@@ -1,1 +1,3 @@
-docker run --rm --volume="$PWD":/srv/jekyll -it -p 4000:4000 -e JEKYLL_ENV=dev jekyll/jekyll:pages jekyll s --config _config.yml,_config.dev.yml --force_polling
+docker run --rm --volume="$PWD":/srv/jekyll -it -p 4000:4000 `
+--name jekyll_robgrzel.github.io -e JEKYLL_ENV=dev `
+jekyll/jekyll jekyll s --config _config.yml,_config.dev.yml --force_polling
